c9754ed695a19109b3c5662932f250ffc3570379
Filipe Lins <filipeallins@gmail.com>
2020-10-29 00:33:40 -0300

    Iniciando criação dos campos e classes referentes aos Beneficiários.

---
+++ ProgPleno/FI.WebAtividadeEntrevista/FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace WebAtividadeEntrevista.Models
+{
+    public class BeneficiarioModel
+    {
+        public long Id { get; set; }
+
+        public long IdCliente { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        public string CPF { get; set; }
+    }
+}
