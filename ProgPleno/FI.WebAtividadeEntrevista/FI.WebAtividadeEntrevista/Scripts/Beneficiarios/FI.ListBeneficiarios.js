5c1df700778c6af820a7edeb52007e35b9100036
Filipe Lins <filipeallins@gmail.com>
2020-10-30 03:05:50 -0300

    Limpeza do script

--- ProgPleno/FI.WebAtividadeEntrevista/FI.WebAtividadeEntrevista/Scripts/Beneficiarios/FI.ListBeneficiarios.js
+++ ProgPleno/FI.WebAtividadeEntrevista/FI.WebAtividadeEntrevista/Scripts/Beneficiarios/FI.ListBeneficiarios.js
@@ -21,41 +21,7 @@ function PesqBeneficiario() {
             }
         },
         error: function () {
-            /* ...show an error... */
+            var newHtml = "Não foi possível carregar os beneficiários";
         }
     });
-
-    //$('#gridBeneficiarios').jtable({
-    //    paging: true, //Enable paging
-    //    pageSize: 5, //Set page size (default: 10)
-    //    sorting: true, //Enable sorting
-    //    defaultSorting: 'Nome ASC', //Set default sorting
-    //    actions: {
-    //        listAction: urlBeneficiarioList,
-    //    },
-    //    fields: {
-    //        CPF: {
-    //            title: 'CPF',
-    //            width: '40%'
-    //        },
-    //        Nome: {
-    //            title: 'Nome',
-    //            width: '40%'
-    //        },
-    //        Alterar: {
-    //            title: '',
-    //            display: function (data) {
-    //                return '<button class="btn btn-primary btn-sm">Alterar</button>';
-    //            }
-    //        },
-    //        Excluir: {
-    //            title: '',
-    //            display: function (data) {
-    //                return '<button class="btn btn-primary btn-sm">Excluir</button>';
-    //            }
-    //        }
-    //    }
-    //});
-
-    //$('#gridBeneficiarios').jtable('load');
 }
